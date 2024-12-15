af2cfc631c4885076c44259b80e6721756eb4e40
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2024-12-15 16:43:07 +0900

    リファクタリング

--- src/components/SettingModal/DBChangeModal/DBChange.tsx
+++ src/components/SettingModal/DBChangeModal/DBChange.tsx
@@ -1,42 +1,34 @@
-import React, { useState } from "react";
+import { UserInfo } from "../../../game/types";
 import Btn from "../../Btn";
 
-type UserInfo = {
-    name: string;
-    chip: number;
-    isDB: boolean; // DB (Dealer Button) 状態を示す
-};
-
 type Props = {
-    users: UserInfo[]; // ユーザー一覧
-    updateUsers: (updatedUsers: UserInfo[]) => void; // ユーザー情報を更新する関数
+    users: Readonly<Pick<UserInfo, "name" | "role">>[]; // ユーザー一覧
+    setRole: (index: number) => void; // ユーザー情報を更新する関数
 };
 
-const DBChange: React.FC<Props> = ({ users, updateUsers }) => {
-    // DB を任意のユーザーに付加する関数
-    const assignDB = (index: number) => {
-        const updatedUsers = users.map((user, i) => ({
-            ...user,
-            isDB: i === index, // 選択されたユーザーのみ DB に設定
-        }));
-        updateUsers(updatedUsers);
-    };
+const DBChange: React.FC<Props> = ({ users, setRole }) => {
+    const userInfos = users.map((user) => ({
+        name: user.name,
+        isDB: user.role === "DB",
+    }));
 
     return (
         <div className="p-4 w-full h-full bg-gray-100 flex flex-col gap-4">
             <h1 className="text-2xl font-bold">Assign Dealer Button (DB)</h1>
             <ul className="flex flex-col gap-3">
-                {users.map((user, index) => (
+                {userInfos.map((user, index) => (
                     <li
                         key={index}
                         className="flex justify-between items-center bg-white p-2 rounded shadow"
                     >
-                        <div className="text-lg">
-                            {user.name}{" "}
+                        <div className="flex gap-3 text-lg">
+                            <span className="text-gray900 after:content-[' ']">
+                                {user.name}
+                            </span>
                             {user.isDB && <span className="text-green-500">(DB)</span>}
                         </div>
                         <Btn
-                            onClick={() => assignDB(index)}
+                            onClick={() => setRole(index)}
                             className={`w-24 ${
                                 user.isDB
                                     ? "bg-gray-400 cursor-not-allowed"
