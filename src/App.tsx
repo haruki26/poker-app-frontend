28026a276921a434d9cfa18a97bc2604f50474f7
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-18 11:41:09 +0900

    コンフリクトの解消

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./Layout";
-import LoginPage from "./pages/Login";
 
 const App: React.FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path="login" element={<LoginPage />} />
             </Route>
         </Routes>
     );
