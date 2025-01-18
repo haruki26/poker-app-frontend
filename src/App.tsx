8f260ecc5698e4da4361698948c87866723c3934
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-18 10:42:29 +0900

    pathにログインページを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./Layout";
+import LoginPage from "./pages/Login";
 
 const App: React.FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
+                <Route path="login" element={<LoginPage />} />
             </Route>
         </Routes>
     );
