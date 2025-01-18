c0c1c4ede734d0ca72cd447d2b95167dde7b1abf
Itsuki0016 <167990332+Itsuki0016@users.noreply.github.com>
2025-01-18 11:10:26 +0900

    Pot Rate cheange font-size

--- src/components/GameInfo/Label.tsx
+++ src/components/GameInfo/Label.tsx
@@ -6,8 +6,8 @@ type Props = {
 const Label: React.FC<Props> = ({ label, value }) => {
     return (
         <div className="flex flex-col gap-2 justify-center items-center p-2">
-            <span className="text-3xl font-bold">{label}</span>
-            <span className="text-5xl font-bold">{value}</span>
+            <span className="text-5xl font-bold">{label}</span>
+            <span className="text-6xl font-bold">{value}</span>
         </div>
     );
 };
