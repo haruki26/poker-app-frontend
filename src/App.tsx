477d4ff02de5aeaea742c8499d72c293e2bce77e
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-18 11:43:10 +0900

    ログインページをパスに再追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./Layout";
 import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/Login";
 
 const App: React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
                 <Route path="/main" element={<MainPage />} />
+                <Route path="/login" element={<LoginPage />} />
             </Route>
         </Routes>
     );
